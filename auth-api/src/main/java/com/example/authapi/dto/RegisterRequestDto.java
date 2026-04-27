908508e623ea52b26c5e2ac3bab87dced34af1ad
oksana <ostroginskaa@gmail.com>
2026-04-27 23:18:17 +0300

    feat: implement DTOs for authentication requests and responses

---
+++ auth-api/src/main/java/com/example/authapi/dto/RegisterRequestDto.java
@@ -0,0 +1,11 @@
+package com.example.authapi.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterRequestDto {
+    private String email;
+    private String password;
+}
